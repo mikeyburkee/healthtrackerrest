78dbeb34b806864f29c51abdd508143865d5e512
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-10-19 18:34:26 +0100

    Add OpenAPI capabilities to JavalinConfig and depenedcny to POM

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -3,12 +3,20 @@ package ie.setu.config
 import ie.setu.controllers.HealthTrackerController
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.ReDocOptions
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.swagger.v3.oas.models.info.Info
 
 class JavalinConfig {
 
     fun startJavalinService(): Javalin {
 
-        val app = Javalin.create().apply {
+        val app = Javalin.create {
+            it.registerPlugin(getConfiguredOpenApiPlugin())
+            it.defaultContentType = "application/json"
+        }.apply {
             exception(Exception::class.java) { e, _ -> e.printStackTrace() }
             error(404) { ctx -> ctx.json("404 - Not Found") }
         }.start(getHerokuAssignedPort())
@@ -40,4 +48,18 @@ class JavalinConfig {
             }
         }
     }
+
+    fun getConfiguredOpenApiPlugin() = OpenApiPlugin(
+        OpenApiOptions(
+            Info().apply {
+                title("Health Tracker App")
+                version("1.0")
+                description("Health Tracker API")
+            }
+        ).apply {
+            path("/swagger-docs") // endpoint for OpenAPI json
+            swagger(SwaggerOptions("/swagger-ui")) // endpoint for swagger-ui
+            reDoc(ReDocOptions("/redoc")) // endpoint for redoc
+        }
+    )
 }
