15c1dcd1d11d244f58a6f8de102c40bbb0988de3
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-10-19 19:08:19 +0100

    Add OpenAPI to updateUserById Method

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -5,11 +5,20 @@ import ie.setu.domain.repository.UserDAO
 import io.javalin.http.Context
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import io.javalin.plugin.openapi.annotations.*
 
 object HealthTrackerController {
 
     private val userDao = UserDAO()
 
+    @OpenApi(
+        summary = "Get all users",
+        operationId = "getAllUsers",
+        tags = ["User"],
+        path = "/api/users",
+        method = HttpMethod.GET,
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<User>::class)])]
+    )
     fun getAllUsers(ctx: Context) {
         ctx.json(userDao.getAll())
     }
@@ -39,6 +48,15 @@ object HealthTrackerController {
         userDao.delete(ctx.pathParam("user-id").toInt())
     }
 
+    @OpenApi(
+        summary = "Update user by ID",
+        operationId = "updateUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.PATCH,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("204")]
+    )
     fun updateUser(ctx: Context){
         val userId = ctx.pathParam("user-id").toInt()
         val foundUser = userDao.findById(userId)
