a6ac7093af99bb68627e736c3ede56a5982386d1
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-10-25 20:03:29 +0100

    Add database map for an activity

--- src/main/kotlin/ie/setu/utils/DBUtilities.kt
+++ src/main/kotlin/ie/setu/utils/DBUtilities.kt
@@ -3,9 +3,20 @@ package ie.setu.utils
 import ie.setu.domain.User
 import ie.setu.domain.db.Users
 import org.jetbrains.exposed.sql.ResultRow
+import ie.setu.domain.Activity
+import ie.setu.domain.db.Activities
 
 fun mapToUser(it: ResultRow) = User(
     id = it[Users.id],
     name = it[Users.name],
     email = it[Users.email]
+)
+
+fun mapToActivity(it: ResultRow) = Activity(
+    id = it[Activities.id],
+    description = it[Activities.description],
+    duration = it[Activities.duration],
+    started = it[Activities.started],
+    calories = it[Activities.calories],
+    userId = it[Activities.userId]
 )
