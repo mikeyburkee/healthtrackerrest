f85dd9948d6ab489f674c8e14cbcb29210abc930
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-10-30 18:26:16 +0000

    Add json object mapper functions to allow serializaition and wrapping

--- src/main/kotlin/ie/setu/utils/JSONUtilities.kt
+++ src/main/kotlin/ie/setu/utils/JSONUtilities.kt
@@ -3,11 +3,35 @@ package ie.setu.utils
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import kong.unirest.HttpResponse
+import kong.unirest.JsonNode
 
-fun mapJSONDate() = (
+
+inline fun mapJSONDate() = (
     jacksonObjectMapper()
     .registerModule(JodaModule())
     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 )
 
+inline fun <reified T: Any> jsonToObject(json: String) : T
+        = jacksonObjectMapper()
+    .registerModule(JodaModule())
+    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+    .readValue<T>(json)
+
+inline fun <reified T: Any>  jsonNodeToObject(jsonNode : HttpResponse<JsonNode>) : T {
+    return jsonToObject<T>(jsonNode.body.toString())
+}
+
+fun jsonObjectMapper(): ObjectMapper
+        = ObjectMapper()
+    .registerModule(JavaTimeModule())
+    .registerModule(JodaModule())
+    .registerModule(KotlinModule.Builder().build())
+    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
