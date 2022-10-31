8e00eb8988ea0221ad543b7f18b412b48ecd0afe
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-10-31 10:12:39 +0000

    Add Mood data class

---
+++ src/main/kotlin/ie/setu/domain/Mood.kt
@@ -0,0 +1,9 @@
+package ie.setu.domain
+
+import org.joda.time.DateTime
+
+data class Mood (var id: Int,
+                     var description:String,
+                     var mood_value: Int,
+                     var logged: DateTime,
+                     var userId: Int)
