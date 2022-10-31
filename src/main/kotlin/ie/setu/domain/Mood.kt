ff226a5ad680578b0375a66844a57abb638428d1
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-10-31 12:30:46 +0000

    Tidy presentation

--- src/main/kotlin/ie/setu/domain/Mood.kt
+++ src/main/kotlin/ie/setu/domain/Mood.kt
@@ -2,7 +2,7 @@ package ie.setu.domain
 
 import org.joda.time.DateTime
 
-data class Mood (var id: Int,
+data class Mood     (var id: Int,
                      var description:String,
                      var mood_value: Int,
                      var logged: DateTime,
