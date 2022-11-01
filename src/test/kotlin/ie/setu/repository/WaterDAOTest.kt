e157f2e2e5205d63572ea144e696e43d3f88ffb2
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-11-01 21:36:00 +0000

    Add read inner class - fully working

--- src/test/kotlin/ie/setu/repository/WaterDAOTest.kt
+++ src/test/kotlin/ie/setu/repository/WaterDAOTest.kt
@@ -1,9 +1,13 @@
 package ie.setu.repository
 
+import ie.setu.domain.db.Waters
+import ie.setu.domain.repository.WaterDAO
+import ie.setu.helpers.populateWaterTable
 import ie.setu.helpers.populateWaterTable
 import ie.setu.helpers.populateUserTable
 import ie.setu.helpers.waters
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Nested
@@ -41,6 +45,81 @@ class WaterDAOTest {
                 assertEquals(water1, waterDAO.findByWaterId(water1.id))
                 assertEquals(water2, waterDAO.findByWaterId(water2.id))
                 assertEquals(water3, waterDAO.findByWaterId(water3.id))
+            }
+        }
+    }
+
+    @Nested
+    inner class ReadWaters {
+
+        @Test
+        fun `getting all activites from a populated table returns all rows`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three waters
+                val userDAO = populateUserTable()
+                val waterDAO = populateWaterTable()
+                //Act & Assert
+                assertEquals(3, waterDAO.getAll().size)
+            }
+        }
+
+        @Test
+        fun `get water by user id that has no waters, results in no record returned`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three waters
+                val userDAO = populateUserTable()
+                val waterDAO = populateWaterTable()
+                //Act & Assert
+                assertEquals(0, waterDAO.findByUserId(3).size)
+            }
+        }
+
+        @Test
+        fun `get water by user id that exists, results in a correct activitie(s) returned`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three waters
+                val userDAO = populateUserTable()
+                val waterDAO = populateWaterTable()
+                //Act & Assert
+                assertEquals(water1, waterDAO.findByUserId(1).get(0))
+                assertEquals(water2, waterDAO.findByUserId(1).get(1))
+                assertEquals(water3, waterDAO.findByUserId(2).get(0))
+            }
+        }
+
+        @Test
+        fun `get all waters over empty table returns none`() {
+            transaction {
+
+                //Arrange - create and setup waterDAO object
+                SchemaUtils.create(Waters)
+                val waterDAO = WaterDAO()
+
+                //Act & Assert
+                assertEquals(0, waterDAO.getAll().size)
+            }
+        }
+
+        @Test
+        fun `get water by water id that has no records, results in no record returned`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three waters
+                val userDAO = populateUserTable()
+                val waterDAO = populateWaterTable()
+                //Act & Assert
+                assertEquals(null, waterDAO.findByWaterId(4))
+            }
+        }
+
+        @Test
+        fun `get water by water id that exists, results in a correct water returned`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three waters
+                val userDAO = populateUserTable()
+                val waterDAO = populateWaterTable()
+                //Act & Assert
+                assertEquals(water1, waterDAO.findByWaterId(1))
+                assertEquals(water3, waterDAO.findByWaterId(3))
             }
         }
     }
