7e7f10dfa5b54f712df00ea99ba4345796ea3862
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-11-04 20:09:13 +0000

    Add weight, height, age and gender attributes

--- src/main/kotlin/ie/setu/domain/db/Users.kt
+++ src/main/kotlin/ie/setu/domain/db/Users.kt
@@ -9,4 +9,8 @@ object Users : Table("users") {
     val id = integer("id").autoIncrement().primaryKey()
     val name = varchar("name", 100)
     val email = varchar("email", 255)
+    val weight = double("weight")
+    val height = double("height")
+    val age = integer("age")
+    val gender = varchar("gender", 10)
 }
