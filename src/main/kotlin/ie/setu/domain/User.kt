d41209ddeb8756c8aabb2caa383c00a5392d562d
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-11-04 19:21:02 +0000

    Add weight, height, age and gender attributes

--- src/main/kotlin/ie/setu/domain/User.kt
+++ src/main/kotlin/ie/setu/domain/User.kt
@@ -2,4 +2,8 @@ package ie.setu.domain
 
 data class User (var id: Int,
                  var name: String,
-                 var email: String)
+                 var email: String,
+                 var weight: Double,
+                 var height: Double,
+                 var age: Int,
+                 var gender: String)
