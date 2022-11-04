1f939297dbbdb6eebc70c7eaaa5c8e21f29053ab
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-11-04 20:11:40 +0000

    Add weight, height, age and gender attributes for update testing

--- src/test/kotlin/ie/setu/repository/UserDAOTest.kt
+++ src/test/kotlin/ie/setu/repository/UserDAOTest.kt
@@ -3,9 +3,7 @@ package ie.setu.repository
 import ie.setu.domain.db.Users
 import ie.setu.domain.User
 import ie.setu.domain.repository.UserDAO
-import ie.setu.helpers.nonExistingEmail
-import ie.setu.helpers.populateUserTable
-import ie.setu.helpers.users
+import ie.setu.helpers.*
 import junit.framework.TestCase.assertEquals
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -142,21 +140,21 @@ class UserDAOTest {
                 val userDAO = populateUserTable()
 
                 //Act & Assert
-                val user3Updated = User(3, "new username", "new@email.ie")
+                val user3Updated = User(3, updatedName, updatedEmail, updatedWeight, updatedHeight, updatedAge, updatedGender)
                 userDAO.update(user3.id, user3Updated)
                 assertEquals(user3Updated, userDAO.findById(3))
             }
         }
 
         @Test
-        fun `updating non-existant user in table results in no updates`() {
+        fun `updating non-existent user in table results in no updates`() {
             transaction {
 
                 //Arrange - create and populate table with three users
                 val userDAO = populateUserTable()
 
                 //Act & Assert
-                val user4Updated = User(4, "new username", "new@email.ie")
+                val user4Updated = User(4, updatedName, updatedEmail, updatedWeight, updatedHeight, updatedAge, updatedGender)
                 userDAO.update(4, user4Updated)
                 assertEquals(null, userDAO.findById(4))
                 assertEquals(3, userDAO.getAll().size)
