b5a461b87e9403352034b2f590d3918e3cdc77b4
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-11-05 11:56:46 +0000

    Add is gender and email validation functions for user validation

--- src/main/kotlin/ie/setu/utils/ValidationUtility.kt
+++ src/main/kotlin/ie/setu/utils/ValidationUtility.kt
@@ -1,8 +1,9 @@
 package ie.setu.utils
 
+import ie.setu.domain.Activity
 import ie.setu.domain.User
 import ie.setu.domain.Water
-import kotlin.math.max
+import java.util.regex.Pattern
 
 val minAge =0
 val maxAge = 140
@@ -12,6 +13,22 @@ val minWeight = 5.0
 val maxWeight = 1000.0
 val minVolume = 0.0
 val maxVolume = 15.0
+val validGenders = listOf('F', 'M', 'O')
+
+fun isValidGender(genderToValidate: String): Boolean {
+    return genderToValidate.get(0).uppercaseChar() in validGenders
+}
+
+fun isValidEmail(email: String): Boolean {
+    val emailRegex = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+"
+    return Pattern.matches(emailRegex, email)
+}
 
 fun doubleIsValidInRange(minValue: Double, maxValue: Double, valueToCheck: Double): Boolean{
     return (valueToCheck >= minValue) && (valueToCheck <= maxValue)
@@ -28,5 +45,9 @@ fun waterInputValidation(water: Water): Boolean{
 fun userInputValidation(user: User): Boolean{
     return intIsValidInRange(minAge, maxAge, user.age) &&
             doubleIsValidInRange(minWeight, maxWeight, user.weight) &&
-            doubleIsValidInRange(minHeight, maxHeight, user.height)
+            doubleIsValidInRange(minHeight, maxHeight, user.height) &&
+            isValidGender(user.gender) &&
+            isValidEmail(user.email)
 }
+
+
