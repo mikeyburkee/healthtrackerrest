eb48477593115d35228754d83dfafa7276d123f0
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-11-05 10:36:10 +0000

    Add functions for returning calories and duration sorted by descending

--- src/main/kotlin/ie/setu/controllers/ActivityController.kt
+++ src/main/kotlin/ie/setu/controllers/ActivityController.kt
@@ -36,13 +36,13 @@ object ActivityController {
         summary = "Get all activities sorted by parameter input in route ",
         operationId = "getAllActivitiesSorted",
         tags = ["Activity"],
-        path = "/api/activities/{sortedBy}",
+        path = "/api/activities/ascending/{sort-param}",
         method = HttpMethod.GET,
         pathParams = [OpenApiParam("sort-param", String::class, "The parameter to sort")],
         responses = [OpenApiResponse("200", [OpenApiContent(Array<Activity>::class)])]
     )
-    fun getAllActivitiesSorted(ctx: Context) {
-        val activities = activityDAO.getAllSorted(ctx.pathParam("sort-param").toString())
+    fun getAllActivitiesSortedAscending(ctx: Context) {
+        val activities = activityDAO.getAllSortedAscending(ctx.pathParam("sort-param"))
         if (activities.size != 0) {
             ctx.status(200)
         }
@@ -53,15 +53,16 @@ object ActivityController {
     }
 
     @OpenApi(
-        summary = "Get all activities sorted by duration  ",
-        operationId = "getAllActivitiesSorted",
+        summary = "Get all activities sorted by parameter descending input in route ",
+        operationId = "getAllActivitiesSortedDescending",
         tags = ["Activity"],
-        path = "/api/activities/duration/sorted",
+        path = "/api/activities/descending/{sort-param}",
         method = HttpMethod.GET,
+        pathParams = [OpenApiParam("sort-param", String::class, "The parameter to sort")],
         responses = [OpenApiResponse("200", [OpenApiContent(Array<Activity>::class)])]
     )
-    fun getAllActivitiesSortedByDuration(ctx: Context) {
-        val activities = activityDAO.getAllSorted("duration")
+    fun getAllActivitiesSortedDescending(ctx: Context) {
+        val activities = activityDAO.getAllSortedDescending(ctx.pathParam("sort-param"))
         if (activities.size != 0) {
             ctx.status(200)
         }
@@ -72,15 +73,72 @@ object ActivityController {
     }
 
     @OpenApi(
-        summary = "Get all activities sorted by calories  ",
-        operationId = "getAllActivitiesSortedByCalories",
+        summary = "Get all activities sorted by ascending duration  ",
+        operationId = "getAllActivitiesSortedAscendingByDuration",
         tags = ["Activity"],
-        path = "/api/activities/calories/sorted",
+        path = "/api/activities/ascending/duration",
         method = HttpMethod.GET,
         responses = [OpenApiResponse("200", [OpenApiContent(Array<Activity>::class)])]
     )
-    fun getAllActivitiesSortedByCalories(ctx: Context) {
-        val activities = activityDAO.getAllSorted("calories")
+    fun getAllActivitiesSortedAscendingByDuration(ctx: Context) {
+        val activities = activityDAO.getAllSortedAscending("duration")
+        if (activities.size != 0) {
+            ctx.status(200)
+        }
+        else{
+            ctx.status(404)
+        }
+        ctx.json(activities)
+    }
+
+    @OpenApi(
+        summary = "Get all activities sorted by descending duration  ",
+        operationId = "getAllActivitiesSortedDescendingByDuration",
+        tags = ["Activity"],
+        path = "/api/activities/descending/duration",
+        method = HttpMethod.GET,
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<Activity>::class)])]
+    )
+    fun getAllActivitiesSortedDescendingByDuration(ctx: Context) {
+        val activities = activityDAO.getAllSortedDescending("duration")
+        if (activities.size != 0) {
+            ctx.status(200)
+        }
+        else{
+            ctx.status(404)
+        }
+        ctx.json(activities)
+    }
+
+    @OpenApi(
+        summary = "Get all activities sorted by ascending calories  ",
+        operationId = "getAllActivitiesSortedAscendingByCalories",
+        tags = ["Activity"],
+        path = "/api/activities/ascending/calories",
+        method = HttpMethod.GET,
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<Activity>::class)])]
+    )
+    fun getAllActivitiesSortedAscendingByCalories(ctx: Context) {
+        val activities = activityDAO.getAllSortedAscending("calories")
+        if (activities.size != 0) {
+            ctx.status(200)
+        }
+        else{
+            ctx.status(404)
+        }
+        ctx.json(activities)
+    }
+
+    @OpenApi(
+        summary = "Get all activities sorted by descending calories  ",
+        operationId = "getAllActivitiesSortedDescendingByCalories",
+        tags = ["Activity"],
+        path = "/api/activities/descending/calories",
+        method = HttpMethod.GET,
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<Activity>::class)])]
+    )
+    fun getAllActivitiesSortedDescendingByCalories(ctx: Context) {
+        val activities = activityDAO.getAllSortedDescending("calories")
         if (activities.size != 0) {
             ctx.status(200)
         }
@@ -115,8 +173,9 @@ object ActivityController {
         }
     }
 
+
     /*
-    TODO sort activities by a singular user
+    //TODO fix issue with  activities by a singular user
     @OpenApi(
         summary = "Get activities by user ID in duration ascending order",
         operationId = "getActivitiesByUserIdInDurationAscendingOrder",
@@ -129,10 +188,6 @@ object ActivityController {
     fun getActivitiesByUserIdWithDurationSorted(ctx: Context){
         if (userDao.findById(ctx.pathParam("user-id").toInt()) != null) {
 
-            var sortType =  ctx.pathParam("sort")
-            if (sortType = "ascending") {
-
-            }
             val activities = activityDAO.findByUserId(ctx.pathParam("user-id").toInt())
             if (activities.isNotEmpty()) {
                 ctx.json(activities)
@@ -146,9 +201,8 @@ object ActivityController {
             ctx.status(404)
         }
     }
-
-
      */
+
     @OpenApi(
         summary = "Add Activity",
         operationId = "addActivity",
