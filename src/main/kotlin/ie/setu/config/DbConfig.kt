3c5486882cb6e2c1a220a42a1e5ef0aae8cd5a0f
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-12-02 21:00:57 +0000

    Updated db config to reflect move of Database and web services

--- src/main/kotlin/ie/setu/config/DbConfig.kt
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -6,10 +6,12 @@ package ie.setu.config
  * @author Michael Burke
  */
 
+import mu.KotlinLogging
 import org.jetbrains.exposed.sql.Database
 
 class DbConfig{
 
+/*
     //NOTE: you need the ?sslmode=require otherwise you get an error complaining about the ssl certificate
     fun getDbConnection() :Database{
         return Database.connect(
@@ -18,5 +20,29 @@ class DbConfig{
             user = "ctenpkjcocylba",
             password = "e88fc0639cb4cdb5d425b7fcfc5d63ebc6524a068dce2756e04a55896a12ad9f")
     }
+*/
+fun getDbConnection() :Database{
 
+    val logger = KotlinLogging.logger {}
+    logger.info{"Starting DB Connection..."}
+
+    val PGUSER = "bfcouqcd"
+    val PGPASSWORD = "hcmgeloj2js3eBzm9Ob95JXemFtgjeRF"
+    val PGHOST = "mouse.db.elephantsql.com"
+    val PGPORT = "5432"
+    val PGDATABASE = "bfcouqcd"
+
+    //url format should be jdbc:postgresql://host:port/database
+    val url = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+
+    val dbConfig = Database.connect(url,
+        driver="org.postgresql.Driver",
+        user = PGUSER,
+        password = PGPASSWORD
+    )
+
+    logger.info{"db url - connection: " + dbConfig.url}
+
+    return dbConfig
+}
 }
--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -30,13 +30,13 @@ class JavalinConfig {
         }.apply {
             exception(Exception::class.java) { e, _ -> e.printStackTrace() }
             error(404) { ctx -> ctx.json("404 - Not Found") }
-        }.start(getHerokuAssignedPort())
+        }.start(getRemoteAssignedPort())
 
         registerRoutes(app)
         return app
     }
 
-    private fun getHerokuAssignedPort(): Int {
+    private fun getRemoteAssignedPort(): Int {
         val herokuPort = System.getenv("PORT")
         return if (herokuPort != null) {
             Integer.parseInt(herokuPort)
