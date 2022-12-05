a8435401a37f7f244322a7819c9fbfdddade3f51
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-12-05 09:56:49 +0000

    Add activities to diplay automatically on footer. Add button to update and delete user

--- src/main/resources/vue/views/user-profile.vue
+++ src/main/resources/vue/views/user-profile.vue
@@ -1,18 +1,58 @@
 <template id="user-profile">
   <app-layout>
-    <div>
-      <form v-if="user">
-        <label class="col-form-label">User ID: </label>
-        <input class="form-control" v-model="user.id" name="id" type="number" readonly/><br>
-        <label class="col-form-label">Name: </label>
-        <input class="form-control" v-model="user.name" name="username" type="text"/><br>
-        <label class="col-form-label">Email: </label>
-        <input class="form-control" v-model="user.email" name="email" type="email"/><br>
-      </form>
-      <dt v-if="user">
-        <br>
-        <a :href="`/users/${user.id}/activities`">View User Activities</a>
-      </dt>
+    <div v-if="noUserFound">
+      <p> We're sorry, we were not able to retrieve this user.</p>
+      <p> View <a :href="'/users'">all users</a>.</p>
+    </div>
+    <div class="card bg-light mb-3" v-if="!noUserFound">
+      <div class="card-header">
+        <div class="row">
+          <div class="col-6"> User Profile </div>
+          <div class="col" align="right">
+            <button rel="tooltip" title="Update"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="updateUser()">
+              <i class="far fa-save" aria-hidden="true"></i>
+            </button>
+            <button rel="tooltip" title="Delete"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="deleteUser()">
+              <i class="fas fa-trash" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <form>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-user-id">User ID</span>
+            </div>
+            <input type="number" class="form-control" v-model="user.id" name="id" readonly placeholder="Id"/>
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-user-name">Name</span>
+            </div>
+            <input type="text" class="form-control" v-model="user.name" name="name" placeholder="Name"/>
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="input-user-email">Email</span>
+            </div>
+            <input type="email" class="form-control" v-model="user.email" name="email" placeholder="Email"/>
+          </div>
+        </form>
+      </div>
+      <div class="card-footer text-left">
+        <p  v-if="activities.length == 0"> No activities yet...</p>
+        <p  v-if="activities.length > 0"> Activities so far...</p>
+        <ul>
+          <li v-for="activity in activities">
+            {{ activity.description }} for {{ activity.duration }} minutes
+          </li>
+        </ul>
+      </div>
     </div>
   </app-layout>
 </template>
@@ -21,14 +61,56 @@
 Vue.component("user-profile", {
   template: "#user-profile",
   data: () => ({
-    user: null
+    user: null,
+    noUserFound: false,
+    activities: [],
   }),
   created: function () {
     const userId = this.$javalin.pathParams["user-id"];
     const url = `/api/users/${userId}`
     axios.get(url)
         .then(res => this.user = res.data)
-        .catch(() => alert("Error while fetching user" + userId));
+        .catch(error => {
+          console.log("No user found for id passed in the path parameter: " + error)
+          this.noUserFound = true
+        })
+    axios.get(url + `/activities`)
+        .then(res => this.activities = res.data)
+        .catch(error => {
+          console.log("No activities added yet (this is ok): " + error)
+        })
+  },
+  methods: {
+    updateUser: function () {
+      const userId = this.$javalin.pathParams["user-id"];
+      const url = `/api/users/${userId}`
+      axios.patch(url,
+          {
+            name: this.user.name,
+            email: this.user.email
+          })
+          .then(response =>
+              this.user.push(response.data))
+          .catch(error => {
+            console.log(error)
+          })
+      alert("User updated!")
+    },
+    deleteUser: function () {
+      if (confirm("Do you really want to delete?")) {
+        const userId = this.$javalin.pathParams["user-id"];
+        const url = `/api/users/${userId}`
+        axios.delete(url)
+            .then(response => {
+              alert("User deleted")
+              //display the /users endpoint
+              window.location.href = '/users';
+            })
+            .catch(function (error) {
+              console.log(error)
+            });
+      }
+    }
   }
 });
 </script>
