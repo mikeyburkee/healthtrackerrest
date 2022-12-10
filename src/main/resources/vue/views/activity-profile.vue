631fbc6657f8ad1dab2bbbcd66264ef1c1ed598e
Michael Burke <michaelpatrick.burke@hotmail.com>
2022-12-10 20:01:13 +0000

    Add card format to profile

--- src/main/resources/vue/views/activity-profile.vue
+++ src/main/resources/vue/views/activity-profile.vue
@@ -1,26 +1,69 @@
 <template id="activity-profile">
   <app-layout>
-    <div>
-      <form v-if="activity">
-        <label class="col-form-label">Activity ID: </label>
-        <input class="form-control" v-model="activity.id" name="id" type="number" readonly/><br>
-        <label class="col-form-label">User Id: </label>
-        <input class="form-control" v-model="activity.userId" name="userId" type="number" readonly/><br>
-        <label class="col-form-label">Name: </label>
-        <input class="form-control" v-model="activity.description" name="activitydescription" type="text"/><br>
-        <label class="col-form-label">Duration (minutes): </label>
-        <input class="form-control" v-model="activity.duration" name="duration" type="number"/><br>
-        <label class="col-form-label">Calories: </label>
-        <input class="form-control" v-model="activity.calories" name="calories" type="number"/><br>
-        <label class="col-form-label">Rating (0-10): </label>
-        <input class="form-control" v-model="activity.rating" name="rating" type="number"/><br>
-        <label class="col-form-label">Date: </label>
-        <input class="form-control" v-model="activity.started" name="started" type="text"/><br>
+    <div class="card bg-light mb-3" v-if="!noActivityFound">
+      <div class="card-header">
+        <div class="row">
+          <div class="col-6"> Activity Profile </div>
+          <div class="col" align="right">
+            <button rel="tooltip" title="Update"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="updateActivity()">
+              <i class="far fa-save" aria-hidden="true"></i>
+            </button>
+            <button rel="tooltip" title="Delete"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="deleteActivity()">
+              <i class="fas fa-trash" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    <div class="card-body">
+      <form>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-id">Activity ID</span>
+          </div>
+          <input type="number" class="form-control" v-model="activity.id" name="activity-id" readonly placeholder="ActivityId"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-id">User ID</span>
+          </div>
+          <input type="number" class="form-control" v-model="activity.userId" name="activity-userid" readonly placeholder="UserID"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-description">Description</span>
+          </div>
+          <input type="text" class="form-control" v-model="activity.description" name="activity-description"  placeholder="Description"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-duration">Duration</span>
+          </div>
+          <input type="number" class="form-control" v-model="activity.duration" name="activity-duration"  placeholder="Duration"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-rating">Rating</span>
+          </div>
+          <input type="number" class="form-control" v-model="activity.rating" name="activity-rating"  placeholder="Rating"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-calories">Calories</span>
+          </div>
+          <input type="number" class="form-control" v-model="activity.calories" name="activity-calories"  placeholder="Calories"/>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="input-activity-date">Date Started</span>
+          </div>
+          <input type="text" class="form-control" v-model="activity.started" name="activity-started"  placeholder="Started"/>
+        </div>
       </form>
-      <dt v-if="activity">
-        <br>
-        <a :href="`/users/${activity.userId}`">View User Profile</a>
-      </dt>
+    </div>
     </div>
   </app-layout>
 </template>
